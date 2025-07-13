797f144a99f1466a87fed92f83896a506cd6d11d
Rainner Kimutai <kimutairainner@gmail.com>
2025-07-14 02:00:25 +0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,46 +1,9 @@
-const CACHE_NAME = "assistant-cache-v1";
-const ASSETS_TO_CACHE = [
-  "/",
-  "/templates/index.html",
-  "/templates/summary.html",
-  "/templates/base.html"
-  "/static/style.css",
-  "/static/script.js",
-  "/manifest.json",
-  "/public/logo.jpg",
-  "/templates/generate.html"
-];
-
-self.addEventListener("install", event => {
-  console.log("[ServiceWorker] Install");
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  console.log("[ServiceWorker] Activate");
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
   self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
 });
